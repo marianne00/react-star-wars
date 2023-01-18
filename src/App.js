590789c64db045d695f35b8351279d9ceac48f6e
Marianne de Asis <marianne@moustacherepublic.com>
2023-01-18 22:33:10 +0800

    add firebase database and send post request

--- src/App.js
+++ src/App.js
@@ -1,66 +1,82 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
-
-  const [movies, setMovies] = useState([])
-  const [isLoading, setisLoading] = useState(false)
-  const [error, setError] = useState('')
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const fetchMoviesHandler = useCallback(async () => {
-    setisLoading(true)
-    setError(null)
-    
+    setIsLoading(true);
+    setError(null);
     try {
-      const response = await fetch('https://swapi.py4e.com/api/films/')
+      const response = await fetch('https://react-star-wars-9df2e-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json');
       if (!response.ok) {
-        throw new Error('Error: ' + response.status + ' ' + response.statusText);
+        throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
-      const transformedMovies = data.results.map((result) => {
-        return {
-          id: result.episode_id,
-          title: result.title,
-          openingText: result.opening_crawl,
-          releaseDate: result.release_date
-        }
-      })
-      setMovies(transformedMovies)
-    } catch(error) {
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+        })
+      }
+
+      setMovies(loadedMovies);
+    } catch (error) {
       setError(error.message);
     }
-    setisLoading(false)
-  }, [])
+    setIsLoading(false);
+  }, []);
 
   useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  async function addMovieHandler(movie) {
+    const response = await fetch('https://react-star-wars-9df2e-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+    console.log(data)
     fetchMoviesHandler()
-  }, [fetchMoviesHandler])
-  
+  }
 
-  // Content
-  let content = "No movies found.";
-  
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
   if (isLoading) {
-    content = <p>Loading...</p>
-  }
-
-  if (!isLoading && movies.length > 0) {
-    content = <MoviesList movies={movies} />
-  }
-
-  if (!isLoading && movies.length < 1 && !error) {
-    content = <p>No movies found.</p>
-  }
-
-  if (!isLoading && error) {
-    content = <p>{error}</p>
+    content = <p>Loading...</p>;
   }
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
       <section>{content}</section>
     </React.Fragment>
   );
