2c522926b1f82548413534bcb8b02ed8d3956e12
Marianne de Asis <marianne@moustacherepublic.com>
2023-01-18 21:53:58 +0800

    add loading and error state

--- src/App.js
+++ src/App.js
@@ -6,22 +6,52 @@ import './App.css';
 function App() {
 
   const [movies, setMovies] = useState([])
+  const [isLoading, setisLoading] = useState(false)
+  const [error, setError] = useState('')
 
-  function fetchMoviesHandler() {
-    fetch('https://swapi.dev/api/films/')
-      .then(response => {
-        return response.json()
-      }).then(data => {
-        const transformedMovies = data.results.map((result) => {
-          return {
-            id: result.episode_id,
-            title: result.title,
-            openingText: result.opening_crawl,
-            releaseDate: result.release_date
-          }
-        })
-        setMovies(transformedMovies)
-      });
+  async function fetchMoviesHandler() {
+    setisLoading(true)
+    setError(null)
+    
+    try {
+      const response = await fetch('https://swapi.py4e.com/api/films/')
+      if (!response.ok) {
+        throw new Error('Error: ' + response.status + ' ' + response.statusText);
+      }
+
+      const data = await response.json();
+      const transformedMovies = data.results.map((result) => {
+        return {
+          id: result.episode_id,
+          title: result.title,
+          openingText: result.opening_crawl,
+          releaseDate: result.release_date
+        }
+      })
+      setMovies(transformedMovies)
+    } catch(error) {
+      setError(error.message);
+    }
+    setisLoading(false)
+  }
+
+  // Content
+  let content = "No movies found.";
+  
+  if (isLoading) {
+    content = <p>Loading...</p>
+  }
+
+  if (!isLoading && movies.length > 0) {
+    content = <MoviesList movies={movies} />
+  }
+
+  if (!isLoading && movies.length < 1 && !error) {
+    content = <p>No movies found.</p>
+  }
+
+  if (!isLoading && error) {
+    content = <p>{error}</p>
   }
 
   return (
@@ -29,9 +59,7 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        <MoviesList movies={movies} />
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
